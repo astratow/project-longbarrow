267452ab7e669a688819715cf946587d755b0fa3
astratow <davno_pora@mail.com>
2023-10-08 06:00:42 +0100

    added footer

--- js/app.js
+++ js/app.js
@@ -5,3 +5,9 @@ const mobileMenuIcon = document.getElementById("mobile-menu");
 mobileMenuIcon.addEventListener("click", function() {
     mobileMenu.style.display = mobileMenu.style.display === "flex" ? "none" : "flex";
 });
+
+let year = new Date().getFullYear();
+// let year = getFullYear();
+console.log(year);
+// let document.querySelector('#year').innerText = year;
+document.querySelector('#year').outerText = year;
