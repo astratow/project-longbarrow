ceb212d5bf0723fba4ce3362e7b5bf1f35e97fdc
astratow <davno_pora@mail.com>
2024-03-03 08:18:23 +0000

    Update:
    >mobile menu not closing bug fix
    >newsletter page
    >newsletter links add to menu
    >application page update (remove form, add download link and text
    suggested)

--- js/app.js
+++ js/app.js
@@ -3,7 +3,9 @@ const mobileMenu = document.getElementById("mobile-menu-list");
 const mobileMenuIcon = document.getElementById("mobile-menu");
 
 mobileMenuIcon.addEventListener("click", function() {
-    mobileMenu.style.display = mobileMenu.style.display === "flex" ? "none" : "flex";
+    console.log('click');
+    // mobileMenu.style.display = mobileMenu.style.display === "flex" ? "none" : "flex";
+    document.querySelector('.nav-menu-mobile').classList.toggle('show');
 });
 
 let year = new Date().getFullYear();
