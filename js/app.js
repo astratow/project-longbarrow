9ab6874de8658eb5b92df9472622927bdf2f07d7
astratow <davno_pora@mail.com>
2025-07-10 10:27:43 +0100

    suggested improvements

--- js/app.js
+++ js/app.js
@@ -1,15 +1,10 @@
 // app.js
-const mobileMenu = document.getElementById("mobile-menu-list");
-const mobileMenuIcon = document.getElementById("mobile-menu");
-
-mobileMenuIcon.addEventListener("click", function() {
-    console.log('click');
-    // mobileMenu.style.display = mobileMenu.style.display === "flex" ? "none" : "flex";
-    document.querySelector('.nav-menu-mobile').classList.toggle('show');
+document.addEventListener('DOMContentLoaded', function() {
+    const mobileMenu = document.getElementById("mobile-menu-list");
+    const mobileMenuIcon = document.getElementById("mobile-menu");
+    mobileMenuIcon.addEventListener("click", function() {
+        mobileMenu.classList.toggle('show');
+    });
+    let year = new Date().getFullYear();
+    document.querySelector('#year').innerText = year;
 });
-
-let year = new Date().getFullYear();
-// let year = getFullYear();
-console.log(year);
-// let document.querySelector('#year').innerText = year;
-document.querySelector('#year').outerText = year;
